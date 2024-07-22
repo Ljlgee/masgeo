e6d739c5678057c850f0d064b22faf446fdb7040
Carrie Lian <13202920401@163.com>
2024-07-22 11:12:45 +0800

    chore: Remove unused ipyleaflet import and commented code

--- masgeo/masgeo.py
+++ masgeo/masgeo.py
@@ -1,8 +1,8 @@
 """Main module."""
 
-import ipyleaflet as ipyl
+# import ipyleaflet as ipyl
 
-class Map(ipyl.Map):
-    def __init__(self, center=[34.540833, 108.923611], zoom=5,**kwargs):
-        super().__init__(center=center,zoom=zoom,**kwargs)
-        self.add_control(ipyl.LayersControl())
+# class Map(ipyl.Map):
+#     def __init__(self, center=[34.540833, 108.923611], zoom=5,**kwargs):
+#         super().__init__(center=center,zoom=zoom,**kwargs)
+#         self.add_control(ipyl.LayersControl())
